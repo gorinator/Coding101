c5168344dac4ae4f7b30bef6ca31d839c0bf51fb
Viktor <151781217+ViktorVoichuk@users.noreply.github.com>
2023-11-23 19:46:53 +0300

    Update task3_typing_speed_tester.cpp

--- task3_typing_speed_tester.cpp
+++ task3_typing_speed_tester.cpp
@@ -21,21 +21,16 @@ int main()
 	//Создаем переменные
 	float a, e, f; 
 	string text;
-
 	cout << "Nazmite lubuiu knopku i vvodite text!" << endl;//Выводим надпись на экран
-
 	system("pause>null"); //пауза на подготовку
-
-	time_t c = time(0);//создаем переменную 'c' с текущим временем в секундах
-
+	time_t c = time(NULL);//создаем переменную 'c' с текущим временем в секундах
 	getline(cin, text);//вводим и считываем из потока текст, и заносим его в меременную text
-
-	time_t d = time(0);//создаем переменную 'd' с текущим временем в секундах
-
-	e = (float)d - c;//вычисляем сколько секунд прошло с момента начала написания текста
+	time_t d = time(NULL);//создаем переменную 'd' с текущим временем в секундах
+	e = d - c;//вычисляем сколько секунд прошло с момента начала написания текста
 	a = text.length();//считываем кол-во символов в переменной text
+	cout << endl;
 	f = (float)(a / e) * 60;//делим кол-во символов на время и умножаем на 60 секунд.
-	cout << "Vi pechataete so skorostiu-" << (float)f << " simvolod v minutu.";//выводим результат в консоль.
+	cout << "Vi pechataete so skorostiu- " << (float)f << " simvolod v minutu." << endl << endl;;//выводим результат в консоль.
 
     system("pause");
 
